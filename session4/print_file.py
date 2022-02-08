7c6783b31baa595b1450d93cc3c173c09074bc1b
Usuario <i.vargas.2021@alumnos.urjc.es>
2022-02-08 16:50:08 +0100

    creation of calculate_weight

---
+++ session4/print_file.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+# -- Constant with the new of the file to open
+FILENAME = "RNU6_269P"  #the file has to be in the same directory
+
+# -- Open and read the file
+file_contents = Path(FILENAME).read_text() #reading the file
+
+# -- Print the contents on the console
+print(file_contents)
