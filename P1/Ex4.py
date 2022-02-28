7dbe505ec813eca37cb1728188e2144906e87247
Usuario <i.vargas.2021@alumnos.urjc.es>
2022-02-28 18:08:15 +0100

    creation of calculate_weight

---
+++ P1/Ex4.py
@@ -0,0 +1,7 @@
+from Seq1 import Seq
+seq1 = Seq("")
+seq2 = Seq("ACTGA")
+seq3 = Seq("Invalid sequence")
+print(f"Sequence : (Length: {seq1.len()}) {seq1}")
+print(f"Sequence : (Length: {seq2.len()}) {seq2}")
+print(f"Sequence : (Length: {seq3.len()}) {seq3}")
