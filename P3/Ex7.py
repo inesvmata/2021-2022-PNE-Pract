7c79dfc49050da948f26475fd587500bfbc11a0c
Usuario <i.vargas.2021@alumnos.urjc.es>
2022-03-22 20:03:00 +0100

    creation of calculate_weight

--- P3/Ex7.py
+++ P3/Ex7.py
@@ -13,10 +13,39 @@ PORT = 8081
 c = Client(IP, PORT)
 
 # -- Test the ping method
+#EXERCISE 1
+print("TESTING PING...")
+response = c.talk("PING")
+print(response)
+
+#EXERCISE 2
 list_get = ["GET 0", "GET 1", "GET 2", "GET 3", "GET4"]
 for e in list_get:
-    print("TESTING", e)
+    print("TESTING", e, "...")
     response = c.talk(e)
+    print(response)
+
+#EXERCISE 3
+print("TESTING INFO...")
+response = c.talk("INFO AACCGGGTTT")
+print(response)
+
+#EXERCISE 4
+print("TESTING COMP...")
+response = c.talk("COMP AACCGGGTTT")
+print(response)
+
+#EXERCISE 5
+print("TESTING REV...")
+response = c.talk("REV AACCGGGTTT")
+print(response)
+
+#EXERCISE 6
+print("TESTING GENE...")
+list_genes = ["U5", "FRAT1", "ADA", "FXN", "RNU6_269P"]
+for g in list_genes:
+    print("TESTING", g, "...")
+    response = c.talk(g)
     print(response)
 
 
